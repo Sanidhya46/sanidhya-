255acadf8af6fb58d0b16db4f5942d2ee84bb15d
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2024-11-13 22:33:46 +0530

    Update 11-fs-async.js

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -7,7 +7,7 @@ console.log('start')
 readFile('./content/subfolder/first.txt','utf8', (err, result) => {
     if (err) {
         console.log(err)
-        return
+        return   
     }
     const first = result;
     readFile('./content/subfolder/first.txt','utf8',(err,result) =>{
