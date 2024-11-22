2dceee8fcbe0897dc6890573ca842a5e3068fa1d
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2024-11-22 22:56:23 +0530

    Update 8-os-module.js

--- 8-os-module.js
+++ 8-os-module.js
@@ -7,7 +7,7 @@ console.log(user);
 //method returns the system uptime in seconds
 console.log(`the System uptime is ${os.uptime()} seconds`)
 
-const currentOs = {
+const currentOs = {   
     name:os.type(),
     release:os.release(),  
     totalMe:os.totalmem(),
