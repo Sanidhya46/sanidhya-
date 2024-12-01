373caf3e6dd03caf2e8b4b488dce5bc4cb87a074
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2024-12-01 23:05:14 +0530

    Update 11-fs-async.js

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -3,7 +3,7 @@
 const { readFile, writeFile } = require('fs');
 
    //This is commit
-console.log('start')
+console.log('start')   
 readFile('./content/subfolder/first.txt','utf8', (err, result) => {
     if (err) {
         console.log(err)  
