d15a93546bbbc0ebed9f1be58ffd86065e79f226
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2024-12-06 11:04:13 +0530

    Update 10-fs-sync.js

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,7 +1,7 @@
 const {readFileSync, writeFileSync} = require('fs');
 console.log('start')
 const first = readFileSync('./content/subfolder/first.txt', 'utf8')
-const second = readFileSync('./content/subfolder/second.txt', 'utf8')
+const second = readFileSync('./content/subfolder/second.txt', 'utf8')   
 
 writeFileSync(
     './content/result-sync.txt',   
