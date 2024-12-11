c4060f91ffec21db95e02cfaca5e709dba05d3d9
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2024-12-11 23:23:58 +0530

    Update 9-path-module.js

--- 9-path-module.js
+++ 9-path-module.js
@@ -4,7 +4,7 @@ console.log(path.sep)
 const filePath = path.join('/content','subfolder', 'test.txt')
 console.log(filePath)  
    
-const base = path.basename(filePath)  
+const base = path.basename(filePath)     
 console.log(base)
 
 const absolute = path.resolve(__dirname,'content','subfolder','test.txt')
