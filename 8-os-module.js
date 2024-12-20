77dcde5a442804ca3d54b44973f89df1dbf4f089
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2024-12-20 23:18:55 +0530

    Update 8-os-module.js

--- 8-os-module.js
+++ 8-os-module.js
@@ -3,7 +3,7 @@ const os = require('os');
 //Info about current user
 const user = os.userInfo()
 console.log(user);
-    
+      
 //method returns the system uptime in seconds   
 console.log(`the System uptime is ${os.uptime()} seconds`)   
 
