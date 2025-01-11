4002496cfda14bda64c37d232b9d3dbba000b5e6
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2025-01-11 10:51:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ setInterval(() => {
     console.log('hello world')
 },1000)*/
   
-/*const http = require('http');
+/*const http = require('http');   
     
 const server = http.createServer((req,res) => {   
    if(req.ulr === '/'){
