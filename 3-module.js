687ac5f413d5b45183373afe3e6a368ba870d57d
Sanidhya46 <168342496+Sanidhya46@users.noreply.github.com>
2025-05-24 21:13:16 +0530

    Update 3-module.js

--- 3-module.js
+++ 3-module.js
@@ -4,7 +4,7 @@ const names = require('./4-names')
 const sayHi = require('./5-utils')
 const data = require('./alternative-flavour')
 require('./7-mind-gnade')     
-     
+        
 console.log(data)   
     
 
